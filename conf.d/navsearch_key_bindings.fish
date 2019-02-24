3b20f2ff25481df97753dc6b27013d3d54cdb4d8
Patrick Chan <patrickchan1999@gmail.com>
2019-02-24 15:17:44 -0500

    Fix detection of whether vim mode is activated to install vim keybindings.

--- conf.d/navsearch_key_bindings.fish
+++ conf.d/navsearch_key_bindings.fish
@@ -1,9 +1,9 @@
 # Default key bindings for inserting path
-if test "$__fish_active_key_bindings" = "fish_vi_key_bindings"
-    bind --preset -M insert \cs ns-skim-insert-from-root
-    bind --preset -M insert \cS ns-skim-insert-from-root
-    bind --preset -M insert \es ns-skim-insert-from-home
-    bind --preset -M insert \eS ns-skim-insert-from-home
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \cs ns-skim-insert-from-root
+    bind -M insert \cS ns-skim-insert-from-root
+    bind -M insert \es ns-skim-insert-from-home
+    bind -M insert \eS ns-skim-insert-from-home
 else
     bind \cs ns-skim-insert-from-root
     bind \cS ns-skim-insert-from-root
@@ -12,11 +12,11 @@ else
 end
 
 # Default key bindings for opening files
-if test "$__fish_active_key_bindings" = "fish_vi_key_bindings"
-    bind --preset -M insert \co ns-skim-open-files-from-root
-    bind --preset -M insert \cO ns-skim-open-files-from-root
-    bind --preset -M insert \eo ns-skim-open-files-from-pwd
-    bind --preset -M insert \eO ns-skim-open-files-from-pwd
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \co ns-skim-open-files-from-root
+    bind -M insert \cO ns-skim-open-files-from-root
+    bind -M insert \eo ns-skim-open-files-from-pwd
+    bind -M insert \eO ns-skim-open-files-from-pwd
 else
     bind \co ns-skim-open-files-from-root
     bind \cO ns-skim-open-files-from-root
@@ -25,9 +25,9 @@ else
 end
 
 # Default key binding for changing directory
-if test "$__fish_active_key_bindings" = "fish_vi_key_bindings"
-    bind --preset -M insert \ec ns-skim-change-directory
-    bind --preset -M insert \eC ns-skim-change-directory
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \ec ns-skim-change-directory
+    bind -M insert \eC ns-skim-change-directory
 else
     bind \ec ns-skim-change-directory
     bind \eC ns-skim-change-directory
